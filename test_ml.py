5b12c309c84661038b1d606c43a21df59e36e9a9
Your Name <you@example.com>
2025-07-21 19:34:58 -0400

    Updated test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,22 +1,46 @@
 import pytest
-import pandas as pd
 import numpy as np
+from ml.model import compute_model_metrics, train_model, inference
+from sklearn.ensemble import RandomForestClassifier
 
-def test_one():
+def test_compute_model_metrics():
     """
-    # Test 
+    Test compute model metrics returns correct values
     """
-   
+    y_true = np.array([1,0,1,0])
+    y_pred = np.array([1,0,1,0])
 
-def test_two():
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    assert recall == 1.0
+    assert precision == 1.0
+    assert fbeta == 1.0
+
+
+def test_train_model():
     """
-    # Test
+    Test trained model is a RandomForestClassifier instance
+    """
+    X_train = np.array([[0,1],[1,0],[0,0],[1,1]])
+    y_train = np.array([0,1,0,1])
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, RandomForestClassifier), "Expected RandomForestClassifier"
+ 
+
+def test_inference():
+    """
+    Test inference returns valid predicted values
     """
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+    X_train = np.array([[0,0],[1,1]])
+    y_train = np.array([0,1])
+    X_test = np.array([[0,1],[1,0]])
+
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train) 
+    
+    preds = inference(model, X_test)
+
+    assert set(preds).issubset({1,0}), "Expected values are 1 and 0"
